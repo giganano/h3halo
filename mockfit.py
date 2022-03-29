9624befee6d1c9cdf61a2d62b1644e933bf5651b
astrobeard <giganano9@gmail.com>
2022-03-29 11:15:07 -0400

    fitting mock sample

--- mockfit.py
+++ mockfit.py
@@ -4,7 +4,7 @@ import numpy as np
 import math as m
 import vice
 from vice.yields.presets import JW20
-vice._dev.set_logging_level("trace")
+# vice._dev.set_logging_level("trace")
 from scipy.stats import multivariate_normal
 from scipy.special import logsumexp
 from utils import cov
@@ -18,9 +18,15 @@ N_WALKERS = 6
 N_DIM = 3
 
 
-class expifr_mcmc():
+class expifr_mcmc(vice.singlezone):
 
-	def __init__(self, data, name = "mockfit"):
+	def __init__(self, data, name = "mockfit", **kwargs):
+		super().__init__(name = name, **kwargs)
+		self.elements = ["fe", "o"]
+		self.func = exponential()
+		self.mode = "ifr"
+		self.Mg0 = 0
+		self.dt = ENDTIME / N_TIMESTEPS
 		self.quantities = list(data.keys())
 		self.quantities = list(filter(lambda x: not x.endswith("_err"),
 			self.quantities))
@@ -39,18 +45,14 @@ class expifr_mcmc():
 
 	def __call__(self, walker):
 		if any([_ < 0 for _ in walker]): return -float("inf")
-		with vice.singlezone(name = "mockfit", verbose = True) as sz:
-			sz.elements = ["fe", "o"]
-			# sz.nthreads = 2
-			sz.func = exponential(timescale = walker[0])
-			sz.mode = "ifr"
-			sz.Mg0 = 0
-			sz.dt = ENDTIME / N_TIMESTEPS
-			sz.tau_star = walker[1]
-			sz.eta = walker[2]
-			out = sz.run(np.linspace(0, ENDTIME, N_TIMESTEPS + 1),
-				overwrite = True, capture = True)
-		sys.stdout.flush()
+		sys.stdout.write("walker: [%.5e, %.5e, %.5e] " % (walker[0],
+			walker[1], walker[2]))
+		self.func.timescale = walker[0]
+		self.tau_star = walker[1]
+		self.eta = walker[2]
+		out = super().run(np.linspace(0, ENDTIME, N_TIMESTEPS + 1),
+			overwrite = True, capture = True)
+		sys.stdout.write("Success!\n")
 		model = np.array([out.history[key][1:] for key in self.quantities]).T
 		weights = out.history["sfr"][1:]
 
@@ -122,7 +124,7 @@ if __name__ == "__main__":
 	# print(p0)
 	start = time.time()
 	# state = sampler.run_mcmc(p0, 1, skip_initial_state_check = True)
-	state = sampler.run_mcmc(p0, 200, skip_initial_state_check = True)
+	state = sampler.run_mcmc(p0, 50, skip_initial_state_check = True)
 	stop = time.time()
 	print("MCMC time: ", stop - start)
 	samples = sampler.get_chain()
