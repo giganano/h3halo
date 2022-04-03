b673d46bd265ffde5f56f8cb6290a3a7c906d8b4
astrobeard <giganano9@gmail.com>
2022-04-03 17:45:15 -0400

    notes on fitting mock samples ; GSE initial run

---
+++ cornerplot.py
@@ -0,0 +1,124 @@
+r"""
+ARGV
+----
+1) The name of the file containing the MCMC data. Log likehood values assumed
+	to be in the far right column.
+2) The name of the output image.
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter as fsf
+from plots.mpltoolkit import load_mpl_presets, named_colors, markers
+import corner
+import numpy as np
+import sys
+load_mpl_presets()
+
+FILENAME = sys.argv[1]
+LABELS = [
+	r"$\tau_\text{in}$ [Gyr]",
+	r"$\tau_\star$ [Gyr]",
+	r"$\eta$"
+]
+# RANGE = None
+RANGE = [
+	# fiducial
+	# (1.88, 2.14),
+	# (8.6, 11.4),
+	# (23.8, 26.2)
+	# precise
+	(1.97, 2.05),
+	(9.5, 10.3),
+	(24.8, 25.2)
+	# imprecise
+	# (1.7, 2.4),
+	# (5, 15),
+	# (22, 28)
+	# small
+	# (1.5, 2.1),
+	# (8, 16),
+	# (23.4, 27.6)
+	# noages
+	# (1.6, 2.3),
+	# (8.4, 12.6),
+	# (23.4, 26)
+	# large
+	# (1.95, 2.05),
+	# (9.6, 11.0),
+	# (24.6, 25.5)
+]
+# TICKS = None
+TICKS = [
+	# fiducial
+	# [1.9, 2.0, 2.1],
+	# [9, 10, 11],
+	# [24, 25, 26]
+	# precise
+	[1.98, 2, 2.02, 2.04],
+	[9.6, 9.8, 10, 10.2],
+	[24.9, 25, 25.1]
+	# imprecise
+	# [1.8, 2, 2.2],
+	# [6, 8, 10, 12, 14],
+	# [23, 25, 27]
+	# small
+	# [1.6, 1.8, 2.0],
+	# [10, 12, 14],
+	# [24, 25, 26, 27]
+	# noages
+	# [1.8, 2.0, 2.2],
+	# [9, 10, 11, 12],
+	# [24, 25, 26]
+	# large
+	# [1.96, 2, 2.04],
+	# [10, 10.5],
+	# [24.6, 25, 25.4]
+]
+MAXLOGP_KWARGS = {
+	"c": named_colors()["deepskyblue"],
+	"marker": markers()["star"],
+	"s": 150,
+	"zorder": 100
+}
+
+raw = np.genfromtxt(FILENAME)
+mcmc_chain = np.array([row[:-1] for row in raw])
+logp = [row[-1] for row in raw]
+idxmax = logp.index(max(logp))
+DIM = len(mcmc_chain[0])
+
+kwargs = {
+	"labels": LABELS,
+	# "quantiles": [0.30, 0.50, 0.70],
+	"quantiles": [0.16, 0.50, 0.84],
+	"show_titles": True,
+	"color": named_colors()["black"],
+	"truths": [2, 10, 25],
+	"truth_color": named_colors()["crimson"],
+}
+if RANGE is not None: kwargs["range"] = RANGE
+fig = corner.corner(mcmc_chain, **kwargs)
+# fig.set_size_inches(15, 15)
+# print(fig.size)
+if TICKS is not None:
+	for i in range(DIM):
+		for j in range(DIM):
+			if i >= j:
+				fig.axes[DIM * i + j].xaxis.set_major_formatter(fsf("%g"))
+				fig.axes[DIM * i + j].tick_params(labelrotation = 0)
+				fig.axes[DIM * i + j].set_xticks(TICKS[j])
+				if i != j:
+					if not j: fig.axes[DIM * i + j].yaxis.set_major_formatter(
+						fsf("%g"))
+					fig.axes[DIM * i + j].set_yticks(TICKS[i])
+					# print(DIM * i + j)
+					# print(mcmc_chain[idxmax][j])
+					# print(mcmc_chain[idxmax][i])
+					fig.axes[DIM * i + j].scatter(
+						mcmc_chain[idxmax][j], mcmc_chain[idxmax][i],
+						**MAXLOGP_KWARGS)
+else: pass
+plt.tight_layout()
+plt.subplots_adjust(hspace = 0, wspace = 0)
+plt.savefig(sys.argv[2])
+
---
+++ expifrfit.py
@@ -0,0 +1,126 @@
+r"""
+ARGV
+----
+1) The name of the file containing the sample
+2) The name of the output file
+3) The name of the VICE output to store each iterations output in
+4) The number of walkers to use
+5) The number of iterations to run each walker through
+6) The number of timesteps to use in each one-zone model integration
+7) The number of burn-in steps to run
+"""
+
+
+from emcee import EnsembleSampler
+from src import fit_driver
+import numpy as np
+import math as m
+import vice
+from vice.yields.presets import JW20
+from utils import cov
+from utils import exponential
+import time
+import sys
+
+ENDTIME = 10
+N_TIMESTEPS = int(sys.argv[6])
+N_WALKERS = int(sys.argv[4])
+N_DIM = 3
+
+
+class expifr_mcmc(vice.singlezone):
+
+	def __init__(self, data, name = sys.argv[3], **kwargs):
+		super().__init__(name = name, **kwargs)
+		self.elements = ["fe", "o"]
+		self.func = exponential()
+		self.mode = "ifr"
+		self.Mg0 = 0
+		self.nthreads = 2
+		self.dt = ENDTIME / N_TIMESTEPS
+
+		self.quantities = list(data.keys())
+		self.quantities = list(filter(lambda x: not x.endswith("_err"),
+			self.quantities))
+		sample = np.array([data[key] for key in self.quantities]).T
+		errors = np.array(
+			[data["%s_err" % (key)] for key in self.quantities]).T
+		invcovs = len(sample) * [None]
+		for i in range(len(sample)):
+			cov = np.diag(errors[i]**2)
+			invcovs[i] = np.linalg.inv(cov)
+
+		self.fd = fit_driver(sample, invcovs)
+
+	def __call__(self, walker):
+		if any([_ < 0 for _ in walker]): return -float("inf")
+		print("walker: [%.5e, %.5e, %.5e] " % (walker[0], walker[1], walker[2]))
+		self.func.timescale = walker[0]
+		self.tau_star = walker[1]
+		self.eta = walker[2]
+		out = super().run(np.linspace(0, ENDTIME, N_TIMESTEPS + 1),
+			overwrite = True, capture = True)
+		model = []
+		for key in self.quantities:
+			if key == "lookback":
+				model.append([m.log10(_) for _ in out.history[key][:-1]])
+			else:
+				model.append(out.history[key][1:])
+		model = np.array(model).T
+		# model = np.array([out.history[key][1:] for key in self.quantities]).T
+		weights = out.history["sfr"][1:]
+		norm = sum(weights)
+		weights = [_ / norm for _ in weights]
+
+		self.fd.model = model
+		self.fd.weights = weights
+
+		result = self.fd()
+		return result
+
+
+if __name__ == "__main__":
+	raw = np.genfromtxt(sys.argv[1])
+	data = {
+		"[fe/h]": raw[:, 0],
+		"[fe/h]_err": raw[:, 1],
+		"[o/fe]": raw[:, 2],
+		"[o/fe]_err": raw[:, 3]
+		# "[fe/h]": np.array([row[0] for row in raw]),
+		# "[fe/h]_err": np.array([row[1] for row in raw]),
+		# "[o/fe]": np.array([row[2] for row in raw]),
+		# "[o/fe]_err": np.array([row[3] for row in raw]),
+		# "lookback": np.array([row[4] for row in raw]),
+		# "lookback_err": np.array([row[5] for row in raw])
+		# "lookback": np.array([m.log10(row[4]) for row in raw]),
+		# "lookback_err": np.array([m.log10(row[5]) for row in raw])
+	}
+	log_prob = expifr_mcmc(data)
+	sampler = EnsembleSampler(N_WALKERS, N_DIM, log_prob)
+	# start initial at known position anyway since this is a mock
+	p0 = N_WALKERS * [None]
+	for i in range(len(p0)):
+		p0[i] = [2, 10, 25]
+		for j in range(len(p0[i])):
+			p0[i][j] += np.random.normal(scale = 0.1 * p0[i][j])
+	p0 = np.array(p0)
+	start = time.time()
+	state = sampler.run_mcmc(p0, int(sys.argv[7]))
+	sampler.reset()
+	state = sampler.run_mcmc(state, int(sys.argv[5]),
+		skip_initial_state_check = True)
+	stop = time.time()
+	print("MCMC time: ", stop - start)
+	samples = sampler.get_chain()
+	logprob = sampler.get_log_prob()
+	samples = np.concatenate(tuple([samples[i] for i in range(len(samples))]))
+	logprob = np.concatenate(tuple([logprob[i] for i in range(len(logprob))]))
+	logprob = [[logprob[_]] for _ in range(len(logprob))]
+	out = np.append(samples, logprob, axis = 1)
+	af = sum(sampler.acceptance_fraction) / N_WALKERS
+	np.savetxt(sys.argv[2], out, fmt = "%.5e",
+		header = "acceptance fraction: %.5e" % (af))
+
+
+
+
