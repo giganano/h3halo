192142a5b666ad01ad1afd41133dff7dd4552a96
astrobeard <giganano9@gmail.com>
2022-04-04 22:15:13 -0400

    mcmc multiprocessing -> fit_driver pickleable

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,8 @@
 
-from ._fit_driver import fit_driver
+# from ._fit_driver import fit_driver
+from . import _fit_driver
+
+class fit_driver(_fit_driver.fit_driver):
+
+	def __reduce__(self):
+		return (self.__class__, (self.sample, self.invcovs, ))
