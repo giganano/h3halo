57535339856b82cb2aca0bac5b5fab4a4f6623d9
James W. Johnson <43220904+giganano@users.noreply.github.com>
2022-06-15 11:01:20 -0400

    mock sample fitting script - command-line args

--- mockfit.py
+++ mockfit.py
@@ -18,16 +18,19 @@ import os
 # DATA_FILE = "./mocksamples/fiducial.dat"
 # OUTFILE = "./mocksamples/fiducial_5000.out"
 # MODEL_BASENAME = "fiducial"
-DATA_FILE = "./mocksamples/n100.dat"
-OUTFILE = "./mocksamples/n100_5000.out"
-MODEL_BASENAME = "n100_"
+# DATA_FILE = "./mocksamples/n100.dat"
+# OUTFILE = "./mocksamples/n100_5000.out"
+# MODEL_BASENAME = "n100_"
+DATA_FILE = sys.argv[1]
+OUTFILE = sys.argv[2]
+MODEL_BASENAME = sys.argv[3]
 
 
 N_PROC = 10
 N_TIMESTEPS = 1000
-N_WALKERS = 50
+N_WALKERS = 256
 N_BURNIN = 100
-N_ITERS = 100
+N_ITERS = 200
 COSMOLOGICAL_AGE = 13.2
 N_DIM = 6
 
