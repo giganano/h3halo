3066cc0a5e619259f51477d9664fbe1b0641188a
James Johnson <giganano9@gmail.com>
2022-07-11 10:40:00 -0400

    Sgr dSph fits

--- sgrfit.sh
+++ sgrfit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --ntasks=40
 #SBATCH --exclusive
 #SBATCH --job-name=sgrfit
 #SBATCH --account=PCON0003
 
 date
-python sgrfit.py ./data/sgr/sgrchem.dat ./data/sgr/sgrchem_25k6.out sgr
+python sgrfit.py ./data/sgr/sgrchem.dat ./data/sgr/sgrchem_102k4.out sgr
 date
 
