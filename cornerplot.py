78a65047315778d1d6e472a40daf6d059be74494
James W. Johnson <giganano9@gmail.com>
2022-08-03 18:34:50 -0400

    paper progress

--- cornerplot.py
+++ cornerplot.py
@@ -16,49 +16,31 @@ load_mpl_presets()
 
 FILENAME = sys.argv[1]
 LABELS = [
-	r"$t_1$",
-	r"$\sigma_1$",
-	r"$A_2 / A_1$",
-	r"$t_2$",
-	r"$\sigma_2$",
+	r"$\tau_\text{in}$ [Gyr]",
 	r"$\eta$",
-	r"$\tau_{\star,0}$",
-	r"$f_\text{burst}$",
-	r"$\tau_\text{tot}$"
-	# r"$\tau_\text{const}$ [Gyr]",
-	# r"$\tau_\text{in}$ [Gyr]",
-	# r"$\eta$",
-	# r"$\tau_\star$ [Gyr]",
-	# r"$\tau_\text{tot}$ [Gyr]",
-	# r"$1000\times y_\text{Fe}^\text{CC}$",
-	# r"$1000\times y_\text{Fe}^\text{Ia}$"
+	r"$\tau_\star$ [Gyr]",
+	r"$\tau_\text{tot}$ [Gyr]",
+	r"$1000\times y_\text{Fe}^\text{CC}$",
+	r"$1000\times y_\text{Fe}^\text{Ia}$"
 ]
-RANGE = None
-# RANGE = [
-	# (1, 3),
-	# (0, 1.5),
-	# (5, 12),
-	# (8, 22),
-	# (4, 7),
-	# (0.6, 0.9),
-	# (0.9, 1.7)
-	###
-	# (9, 15),
-	# (10, 20),
-	# (3, 8),
-	# (0.7, 0.95),
-	# (0.8, 1.3)
-# ]
-TICKS = None
-# TICKS = [
-# 	[1.6, 1.8, 2.0, 2.2],
-# 	[200, 400],
-# 	[200, 400, 600],
-# 	[8, 9, 10, 11],
-# 	[10, 20, 30, 40],
-# 	[10, 20, 30, 40],
-# 	[20, 40, 60]
-# ]
+# RANGE = None
+RANGE = [
+	(0.4, 1.6),
+	(4, 13),
+	(11, 22),
+	(4, 7),
+	(0.62, 0.96),
+	(0.8, 1.7)
+]
+# TICKS = None
+TICKS = [
+	[0.5, 1, 1.5],
+	[6, 8, 10, 12],
+	[15, 20],
+	[5, 6],
+	[0.7, 0.8, 0.9],
+	[1.0, 1.5]
+]
 MAXLOGP_KWARGS = {
 	"c": named_colors()["deepskyblue"],
 	"marker": markers()["star"],
@@ -68,21 +50,23 @@ MAXLOGP_KWARGS = {
 
 raw = np.genfromtxt(FILENAME)
 raw = np.array(list(filter(lambda _: not np.isinf(_[-1]), raw)))
+raw = np.array(list(filter(lambda _: RANGE[0][0] <= _[0] <= RANGE[0][1], raw)))
 mcmc_chain = np.array([row[:-1] for row in raw])
-# for i in range(len(mcmc_chain)):
-# 	mcmc_chain[i][-1] *= 1000
-# 	mcmc_chain[i][-2] *= 1000
+for i in range(len(mcmc_chain)):
+	mcmc_chain[i][-1] *= 1000
+	mcmc_chain[i][-2] *= 1000
 # 	mcmc_chain[i][-3] *= 1000
 logp = [row[-1] for row in raw]
 idxmax = logp.index(max(logp))
 DIM = len(mcmc_chain[0])
+print(mcmc_chain[idxmax])
 
 kwargs = {
 	"labels": LABELS,
 	"quantiles": [0.16, 0.50, 0.84],
 	"show_titles": True,
 	"color": named_colors()["black"],
-	# "truths": mcmc_chain[idxmax],
+	"truths": mcmc_chain[idxmax],
 	# "truths": [2, 10, 15, 10, 0.8, 1.1],
 	# "truths": [2, 10, 15, 10, 0.8, 1.1, 10],
 	"truth_color": named_colors()["crimson"]
