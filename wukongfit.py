39372b406823bb903f8f2278953bd0cdd920099b
James W. Johnson <giganano9@gmail.com>
2022-08-05 13:41:44 -0400

    fitting wukong data -> linear exponential SFH

--- wukongfit.py
+++ wukongfit.py
@@ -16,26 +16,24 @@ import time
 import os
 
 DATA_FILE = "./data/wukong/wukong.dat"
-OUTFILE = "./data/wukong/wukong_constantsfr_512k.out"
+OUTFILE = "./data/wukong/wukong_linexpsfr_102k4.out"
 MODEL_BASENAME = "wukongfit"
 
 
-N_PROC = 40
+N_PROC = 10
 N_TIMESTEPS = 500
 N_WALKERS = 256
-N_BURNIN = 1000
-N_ITERS = 2000
+N_BURNIN = 200
+N_ITERS = 400
 COSMOLOGICAL_AGE = 13.2
-N_DIM = 3
+N_DIM = 4
 
-# emcee walker parameters (exponential IFR)
+# emcee walker parameters (linear-exponential SFR)
 #
-# 0. infall timescale
+# 0. star formation timescale
 # 1. mass loading factor
 # 2. SFE timescale
 # 3. total duration of the model
-# 4. IMF-averaged Fe yield from CCSNe
-# 5. DTD-integrated Fe yield from SNe Ia
 
 # emcee walker parameters (constant SFR)
 #
@@ -57,22 +55,24 @@ class wukongfit(mcmc):
 		self.sz.elements = ["fe", "o"]
 		# self.sz.func = exponential(prefactor = 1000)
 		# self.sz.mode = "ifr"
-		self.sz.func = constantsfr
+		# self.sz.func = constantsfr
+		self.sz.func = linear_exponential(prefactor = 1000)
 		self.sz.mode = "sfr"
 		self.sz.Mg0 = 0
 
 	def __call__(self, walker):
 		if any([_ < 0 for _ in walker]): return -float("inf")
-		if walker[2] > COSMOLOGICAL_AGE: return -float("inf")
-		print("walker: [%.2f, %.2f, %.2f]" % (walker[0], walker[1], walker[2]))
+		if walker[3] > COSMOLOGICAL_AGE: return -float("inf")
+		print("walker: [%.2f, %.2f, %.2f, %.2f]" % (walker[0], walker[1],
+			walker[2], walker[3]))
 		self.sz.name = "%s%s" % (MODEL_BASENAME, os.getpid())
-		# self.sz.func.timescale = walker[0]
-		self.sz.eta = walker[0]
-		self.sz.tau_star = walker[1]
-		self.sz.dt = walker[2] / N_TIMESTEPS
+		self.sz.func.timescale = walker[0]
+		self.sz.eta = walker[1]
+		self.sz.tau_star = walker[2]
+		self.sz.dt = walker[3] / N_TIMESTEPS
 		# vice.yields.ccsne.settings['fe'] = walker[3]
 		# vice.yields.sneia.settings['fe'] = walker[4]
-		output = self.sz.run(np.linspace(0, walker[2], N_TIMESTEPS + 1),
+		output = self.sz.run(np.linspace(0, walker[3], N_TIMESTEPS + 1),
 			overwrite = True, capture = True)
 		model = []
 		for key in self.quantities:
