92e66f8baa096e128db2d894856d7a2c7d5c1d38
James W. Johnson <giganano9@gmail.com>
2022-09-15 16:17:18 -0400

    fitting wukong sample -> multiprocessing pool closed and joined by end of script

--- wukongfit.py
+++ wukongfit.py
@@ -17,17 +17,17 @@ import sys
 import os
 
 DATA_FILE = "./data/wukong/wukong.dat"
-OUTFILE = "./data/wukong/wukong_withyields_102k4.out"
+OUTFILE = "./data/wukong/wukong_512k.out"
 MODEL_BASENAME = "wukongfit"
 
 
-N_PROC = 10
+N_PROC = 40
 N_TIMESTEPS = 500
 N_WALKERS = 256
-N_BURNIN = 200
-N_ITERS = 400
+N_BURNIN = 1000
+N_ITERS = 2000
 COSMOLOGICAL_AGE = 13.2
-N_DIM = 6
+N_DIM = 4
 
 # emcee walker parameters (exponential IFR)
 #
@@ -54,17 +54,18 @@ class wukongfit(mcmc):
 	def __call__(self, walker):
 		if any([_ < 0 for _ in walker]): return -float("inf")
 		if walker[3] > COSMOLOGICAL_AGE: return -float("inf")
-		if walker[4] > 0.01: return -float("inf")
-		if walker[5] > 0.01: return -float("inf")
-		print("walker: [%.2f, %.2f, %.2f, %.2f, %.2e, %.2e]" % (walker[0],
-			walker[1], walker[2], walker[3], walker[4], walker[5]))
+		# if walker[4] > 0.01: return -float("inf")
+		# if walker[5] > 0.01: return -float("inf")
+		print("walker: [%.2f, %.2f, %.2f, %.2f]" % (walker[0],
+			walker[1], walker[2], walker[3]))
+			# walker[1], walker[2], walker[3], walker[4], walker[5]))
 		self.sz.name = "%s%s" % (MODEL_BASENAME, os.getpid())
 		self.sz.func.timescale = walker[0]
 		self.sz.eta = walker[1]
 		self.sz.tau_star = walker[2]
 		self.sz.dt = walker[3] / N_TIMESTEPS
-		vice.yields.ccsne.settings['fe'] = walker[4]
-		vice.yields.sneia.settings['fe'] = walker[5]
+		# vice.yields.ccsne.settings['fe'] = walker[4]
+		# vice.yields.sneia.settings['fe'] = walker[5]
 		output = self.sz.run(np.linspace(0, walker[3], N_TIMESTEPS + 1),
 			overwrite = True, capture = True)
 		model = []
@@ -93,14 +94,16 @@ if __name__ == "__main__":
 	# 	p0[i][3] /= 1000
 	# 	p0[i][4] /= 1000
 	# Confine the yields to a plausible range to begin with
-	for i in range(len(p0)):
-		p0[i][4] /= 1000
-		p0[i][5] /= 1000
+	# for i in range(len(p0)):
+	# 	p0[i][4] /= 1000
+	# 	p0[i][5] /= 1000
 	start = time.time()
 	state = sampler.run_mcmc(p0, N_BURNIN)
 	sampler.reset()
 	state = sampler.run_mcmc(state, N_ITERS)
 	stop = time.time()
+	pool.close()
+	pool.join()
 	print("MCMC time: ", stop - start)
 	savechain(sampler, OUTFILE)
 	sys.stdout.flush()
