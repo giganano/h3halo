b6a5749a06bbe4e69e4561865470e2e27f9ad073
James W. Johnson <43220904+giganano@users.noreply.github.com>
2022-09-28 19:32:34 -0400

    fitting fiducial sample

--- runfits.sh
+++ runfits.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=40
 #SBATCH --exclusive
-#SBATCH --job-name=degeneracy
-#SBATCH --account=PCON0003
+#SBATCH --job-name=fiducial
+#SBATCH --account=PAS1140
 
 # start and end the program by echoing the date and time
 date
